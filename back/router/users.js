5699e5da0d5fbe86274fceaaff23cbd650f71a18
ParkDongGyu <idqkrid@naver.com>
2024-04-14 22:50:23 +0900

    user.js update

--- back/router/users.js
+++ back/router/users.js
@@ -6,7 +6,7 @@ const jwt = require('jsonwebtoken');
 const router = express.Router();
 const { auth } = require("./middlewares");
 
-router.get('/', async (req, res, next) => {
+router.get("/", async (req, res, next) => {
   let token = req.cookies.token;
 
   console.log(token);
@@ -49,12 +49,10 @@ router.get('/', async (req, res, next) => {
       .status(401)
       .json({ isAuth: false, error: true, message: "인증에 실패했습니다.2" });
   }
-
-
-})
+});
 
 // 회원가입
-router.post('/regitsre', async (req, res, next) => {
+router.post("/regitsre", async (req, res, next) => {
   try {
     const saltRounds = 10; // salt rounds 값 설정
     const hashedPassword = await bcrypt.hash(req.body.password, saltRounds);
@@ -64,42 +62,49 @@ router.post('/regitsre', async (req, res, next) => {
       email: req.body.email,
       password: hashedPassword,
     });
-    res.status(200).json({ success: true})
+    res.status(200).json({ success: true });
   } catch (err) {
     console.error(err);
-    res.status(403).json({ fail: "회원가입이 실패했습니다."})
+    res.status(403).json({ fail: "회원가입이 실패했습니다." });
     next(err);
   }
 });
 
 // 로그인
-router.post('/login', async (req, res, next) => {
+router.post("/login", async (req, res, next) => {
   try {
     const user = await User.findOne({ email: req.body.email });
 
     if (!user) {
       return res.status(401).json({
         loginSuccess: false,
-        message: "제공된 이메일에 해당하는 유저가 없습니다."
+        message: "제공된 이메일에 해당하는 유저가 없습니다.",
       });
     }
 
     const isMatch = await bcrypt.compare(req.body.password, user.password);
 
     if (!isMatch) {
-      return res.status(401).json({ loginSuccess: false, message: "비밀번호가 일치하지 않습니다." });
+      return res.status(401).json({
+        loginSuccess: false,
+        message: "비밀번호가 일치하지 않습니다.",
+      });
     }
 
-    const token = jwt.sign({ userId: user._id }, 'secretToken');
+    const token = jwt.sign({ userId: user._id }, "secretToken");
     user.token = token;
     await user.save();
 
-    res.cookie('token', token, { httpOnly: true, secure: false });
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: false,
+      samesite: Lax,
+    });
 
-    res.status(200).json({ loginSuccess: true, userId: user._id})
+    res.status(200).json({ loginSuccess: true, userId: user._id });
   } catch (err) {
     console.error(err);
-    res.status(401).json({ loginSuccess: false})
+    res.status(401).json({ loginSuccess: false });
     next(err);
   }
 });
